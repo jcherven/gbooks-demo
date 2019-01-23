a64390cdf7992653c2272acadccca93c7a276721
James Cherven <jcherven@mail.usf.edu>
2019-01-23 12:26:32 -0500

    Removes containers directory and resturctures containers

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
