16eb0ee94ff2d567ef58e614e2ebf847300d6a44
James Cherven <jcherven@mail.usf.edu>
2019-01-23 16:27:23 -0500

    Adapts project template

---
+++ client/src/pages/saved.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+
+import DeleteBtn from "../components/DeleteBtn";
+import { List, ListItem } from "../components/List";
+
+class Detail extends Component {
+  // state = {
+  //   book: {}
+  // };
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+  // When this component mounts, grab the book with the _id of this.props.match.params.id
+  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+  componentDidMount() {
+    API.getBook(this.props.match.params.id)
+      .then(res => this.setState({ book: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Saved Books</h1>
+            </Jumbotron>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>Saved Books not found :(</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Detail;
---
+++ client/src/pages/search.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+// import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { Input, FormBtn } from "../components/Form";
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Search Google Books</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="search"
+                placeholder="Search by Title, Author, or anything else"
+              />
+              {/*
+                <Input
+                  value={this.state.title}
+                  onChange={this.handleInputChange}
+                  name="title"
+                  placeholder="Title (required)"
+                />
+                <Input
+                  value={this.state.author}
+                  onChange={this.handleInputChange}
+                  name="author"
+                  placeholder="Author (required)"
+                />
+                <TextArea
+                  value={this.state.synopsis}
+                  onChange={this.handleInputChange}
+                  name="synopsis"
+                  placeholder="Synopsis (Optional)"
+                />
+              */}
+              <FormBtn
+                disabled={!(this.state.author && this.state.title)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Search
+              </FormBtn>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
